f4939ad965e5ca09fc4d8088ac9fd97ec3f51761
Jordan Bluehdorn <jbluehdorn@alphabytes.io>
2019-04-16 14:08:56 -0700

    Added functionality to MainActivity

--- app/src/main/java/com/jbluehdorn/termtracker/MainActivity.java
+++ app/src/main/java/com/jbluehdorn/termtracker/MainActivity.java
@@ -1,13 +1,42 @@
 package com.jbluehdorn.termtracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import com.jbluehdorn.termtracker.terms.TermsActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnCurrTerm, btnAllTerms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCurrTerm = findViewById(R.id.btn_curr_term);
+        btnCurrTerm.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                handleBtnCurrTerm(v);
+            }
+        });
+
+        btnAllTerms = findViewById(R.id.btn_all_terms);
+        btnAllTerms.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                handleBtnAllTerms(v);
+            }
+        });
+    }
+
+    private void handleBtnCurrTerm(View v) {
+        Toast.makeText(getApplicationContext(), R.string.no_curr_term_err, Toast.LENGTH_SHORT).show();
+    }
+
+    private void handleBtnAllTerms(View v) {
+        Intent intent = new Intent(this, TermsActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/jbluehdorn/termtracker/terms/TermsActivity.java
@@ -0,0 +1,14 @@
+package com.jbluehdorn.termtracker.terms;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jbluehdorn.termtracker.R;
+
+public class TermsActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acitvity_terms);
+    }
+}
